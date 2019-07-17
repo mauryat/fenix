afbd743970a181ecf31f044dbea98a20362504bd
James Hugman <james@hugman.tv>
2019-07-17 18:24:01 +0100

    Closes #1331 - Filter consecutive history items using simplified URL

--- app/src/main/java/org/mozilla/fenix/library/history/HistoryFragment.kt
+++ app/src/main/java/org/mozilla/fenix/library/history/HistoryFragment.kt
@@ -38,6 +38,7 @@ import org.mozilla.fenix.ext.components
 import org.mozilla.fenix.ext.getHostFromUrl
 import org.mozilla.fenix.ext.nav
 import org.mozilla.fenix.ext.requireComponents
+import org.mozilla.fenix.ext.simplifiedUrl
 import org.mozilla.fenix.mvi.ActionBusFactory
 import org.mozilla.fenix.mvi.getAutoDisposeObservable
 import org.mozilla.fenix.mvi.getManagedEmitter
@@ -258,12 +259,20 @@ class HistoryFragment : Fragment(), BackHandler {
         // Until we have proper pagination, only display a limited set of history to avoid blowing up the UI.
         // See https://github.com/mozilla-mobile/fenix/issues/1393
         val sinceTimeMs = System.currentTimeMillis() - TimeUnit.DAYS.toMillis(HISTORY_TIME_DAYS)
+        var previous: String? = null
+
         val items = requireComponents.core.historyStorage
             .getDetailedVisits(sinceTimeMs, excludeTypes = excludeTypes)
             // We potentially have a large amount of visits, and multiple processing steps.
             // Wrapping iterator in a sequence should make this a little memory-more efficient.
             .asSequence()
             .sortedByDescending { it.visitTime }
+            .filter {
+                val current = it.url.simplifiedUrl()
+                val isNotDuplicate = current != previous
+                previous = current
+                isNotDuplicate
+            }
             .mapIndexed { id, item ->
                 val title = item.title
                     ?.takeIf(String::isNotEmpty)
